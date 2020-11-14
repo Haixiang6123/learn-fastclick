59d690796d45f4fb1fe57d12194cb1147ce53b58
Haixiang6123 <haixiang6123@gmail.com>
2020-11-14 19:10:50 +0800

    feat: 引入 antd 和 fastclick

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import {Select} from "antd";
+
+const { Option } = Select;
 
 function App() {
+  const names = ['Jack', 'Dany', 'Jerry']
+
   return (
     <div>
-      learn fastclick
+      <Select defaultValue="Jack">
+        {names.map((n) => (
+          <Option key={n} value={n}>{n}</Option>
+        ))}
+      </Select>
     </div>
   );
 }
